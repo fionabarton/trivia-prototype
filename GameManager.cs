ef6b94aff0915910632a9aeaf2e291efecba916b
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-12-14 14:48:29 -0500

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public Text             questionText;
+    public List<GameObject> answers;
+
+    [Header("Set Dynamically")]
+    // Index of the question currently displayed
+    public int              currentNdx = 0;
+
+    public List<Text>       answerTexts;
+    public List<Button>     answerButtons;
+
+    // Start is called before the first frame update
+    void Start() {
+        // Get Answer Text and Button components
+        for (int i = 0; i < answers.Count; i++) {
+            answerTexts[i] = answers[i].GetComponentInChildren<Text>();
+            answerButtons[i] = answers[i].GetComponent<Button>();
+        }
+
+        // Set up first question/answers
+        SetQuestions();
+    }
+
+    // Set up current question/answers
+    void SetQuestions() {
+        // Get data for current question
+        Question q = QuestionManager.S.questions[currentNdx];
+
+        // Set question text
+        questionText.text = q.questionString;
+
+        // Set answers text
+        for (int i = 0; i < answers.Count; i++) {
+            answerTexts[i].text = q.answerStrings[i];
+        }
+
+        // Remove listeners from all answer buttons
+        for (int i = 0; i < answerButtons.Count; i++) {
+            answerButtons[i].onClick.RemoveAllListeners();
+        }
+
+        // Set OnClick methods
+        for (int i = 0; i < answers.Count; i++) {
+            if(i == q.correctAnswerNdx) {
+                // If user has selected correct answer...
+                answerButtons[i].onClick.AddListener(delegate { AnswerSelected("<color=green>" + "Correct answer!" + "</color>"); });
+            } else {
+                // If user has selected incorrect answer...
+                answerButtons[i].onClick.AddListener(delegate { AnswerSelected("<color=red>" + "Incorrect answer!" + "</color>"); });
+            }
+        }
+
+        // Make the answer buttons interactable
+        for (int i = 0; i < answerButtons.Count; i++) {
+            answerButtons[i].interactable = true;
+        }
+
+        // Set selected game object to first answer button
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(answers[0].gameObject);
+    }
+
+    // Called when an answer button is selected by the user
+    void AnswerSelected(string messageToDisplay) {
+        // Set question text
+        questionText.text = messageToDisplay;
+
+        // Deactivate the interactability of the answer buttons
+        for (int i = 0; i < answerButtons.Count; i++) {
+            answerButtons[i].interactable = false;
+        }
+
+        // Increment currentNdx to display next question
+        currentNdx = (currentNdx + 1) % QuestionManager.S.questions.Count;
+
+        // Call SetQuestions() in approximately 2 seconds
+        Invoke("SetQuestions", 2);
+    }
+}
---
+++ QuestionManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum eTopic { plasticPollution, deforestation, airPollution, climateChange, other };
+
+public class QuestionManager : MonoBehaviour {
+    [Header("Set Dynamically")]
+    private static QuestionManager  _S;
+    public static QuestionManager   S { get { return _S; } set { _S = value; } }
+
+    public List<Question>           questions = new List<Question>();
+
+    void Awake() {
+        S = this;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        questions.Add(
+            new Question("What colour is the sky?",
+            new List<string> { "Dog", "Apple", "Dragon Ball", "Blue" },
+            eTopic.other, 3));
+
+        questions.Add(
+            new Question("Which of the following people\nis NOT a member of our team?",
+            new List<string> { "Robocop", "Davina", "Siddhant", "Tertius" },
+            eTopic.other, 0));
+
+        questions.Add(
+            new Question("What date is our midterm\nproposal due?",
+            new List<string> { "10 January 2021", "2 September 1945", "10 January 2022", "4 December 1985" },
+            eTopic.other, 2));
+    }
+}
+
+public class Question {
+    public string       questionString;
+    public List<string> answerStrings;
+
+    public eTopic       topic;
+    public int          correctAnswerNdx;
+
+    public bool         answeredCorrectly;
+
+    public Question(string _question, List<string> _answers, eTopic _topic, int _correctAnswerNdx) {
+        questionString = _question;
+        answerStrings = _answers;
+        topic = _topic;
+        correctAnswerNdx = _correctAnswerNdx;
+    }
+}
